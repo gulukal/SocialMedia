922848e8ff319b4b995281103797ef3d26ad56ea
Gulten Ulukal <gulten.ulukal@gmail.com>
2022-02-04 12:37:02 +0300

    aut-service -Dto and Service classes has been created and validation- swagger dependencies have been added

--- auth-service/src/main/java/com/gulukal/service/UserService.java
+++ auth-service/src/main/java/com/gulukal/service/UserService.java
@@ -14,39 +14,43 @@ public class UserService {
     @Autowired
     IUserRepository iUserRepository;
 
-//    Kullaniciyi kayit eder
-//    @param user
-//    @return
+    /**
+     * Kullanıcıyı kayıt eder ve kayıtedilen kullanıcının id bilgisi alınarak geri döndürülür.
+     * @param user
+     * @return
+     */
 
     public User saveReturnUser(User user){
         iUserRepository.save(user);
         return user;
     }
 
-    public void save (User user){
+    public void save(User user){
         iUserRepository.save(user);
     }
 
-    public void update (User user){
+    public void update(User user){
         iUserRepository.save(user);
     }
 
-    public void delete (User user){
+    public void delete(User user){
         iUserRepository.delete(user);
     }
 
-    public List<User> findAll (){
+    public List<User> findAll(){
         return iUserRepository.findAll();
     }
 
-    public Optional<User> findUsernameAndPassword(String username, String password){
-
+    public Optional<User> findByUsernameAndPassword(String username,String password){
         return iUserRepository.findByUsernameAndPassword(username, password);
     }
 
-    public boolean isUser(String username, String password){
-        Optional<User> user = iUserRepository.findByUsernameAndPassword(username, password); {
-            return user.isPresent();
-        }
+    public boolean isUser(String username,String password){
+        Optional<User> user = iUserRepository.findByUsernameAndPassword(username, password);
+        if (user.isPresent())
+            return true;
+        return false;
     }
+
+
 }
