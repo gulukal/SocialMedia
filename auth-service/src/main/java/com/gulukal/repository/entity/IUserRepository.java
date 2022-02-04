dd66d6ede8167c9c1050f8def096af730dc5b8b4
Gulten Ulukal <gulten.ulukal@gmail.com>
2022-02-04 10:34:20 +0300

    IUserRepository has been created

--- auth-service/src/main/java/com/gulukal/repository/entity/IUserRepository.java
+++ auth-service/src/main/java/com/gulukal/repository/entity/IUserRepository.java
@@ -1,4 +1,23 @@
 package com.gulukal.repository.entity;
 
-public interface IUserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+
+    //   kullaninicin oturum acabilmesi icin email ve sifre  bilgisi alinir. kayitli bilgi var ise
+    //   optional olarak cevap donulur.
+    //   @param username - email bilgisi
+    //   @param password - sifre bilgisi
+    //   @return
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
+
+    //   kullanici aktiflik durumunu belirtir
+    //   @param status - email bilgisi
+    //   @return
+
+    List<User> findByStatus(int status);
 }
